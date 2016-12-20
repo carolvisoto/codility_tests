5a62de05ab141c4b01b4d891865bfcb3ee8fc295
Caroline Visoto Nunes <carolvisoto@gmail.com>
2016-12-21 00:36:14 +0100

    complete solution

--- src/main/java/CyclicRotation.java
+++ src/main/java/CyclicRotation.java
@@ -1,5 +1,23 @@
 public class CyclicRotation {
-    public int[] rotateArray(int[] array, int i) {
-        return new int[]{};
+    public int[] rotateArray(int[] array, int number) {
+        for (int i = 0; i < number; i++) {
+            int tmp = array[array.length - 1];
+            for (int j = array.length - 1; j > 0; j--){
+                array[j] = array[j - 1];
+            }
+            array[0] = tmp;
+        }
+        return array;
+    }
+
+    public static void main (String[] args){
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        int[] array = {1,3,5,7,9};
+        int[] rotateArray;
+        int numberTimes = 1;
+        rotateArray = cyclicRotation.rotateArray(array,numberTimes);
+        for (int i = 0; i < rotateArray.length; i++ ){
+            System.out.println(rotateArray[i]);
+        }
     }
 }
